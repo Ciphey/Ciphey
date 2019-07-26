3296a79e762f70e6e896f0ae8ba3187882873dad
brandon <brandonskerritt51@gmail.com>
2019-07-26 01:23:33 +0100

    made dictionary changer work

--- app/Tests/testdictionary_checker.py
+++ app/Tests/testdictionary_checker.py
@@ -10,3 +10,19 @@ class TestChi(unittest.TestCase):
         dc = dictionaryChecker.dictionaryChecker()
         result = dc.confirmlanguage("hello again my friend this is my name and I like dogs!", "English")
         self.assertEqual(result, True)
+    def test_english_yes_two(self):
+        dc = dictionaryChecker.dictionaryChecker()
+        result = dc.confirmlanguage("hello my name is brandon and this is a normal english text timtable fuse kindle hormone", "English")
+        self.assertEqual(result, True) 
+    def test_english_false(self):
+        dc = dictionaryChecker.dictionaryChecker()
+        result = dc.confirmlanguage("jdajj kop9u0r 9jjidasjp", "English")
+        self.assertEqual(result, False)
+    def test_english_false_two(self):
+        dc = dictionaryChecker.dictionaryChecker()
+        result = dc.confirmlanguage("pink jdajj red 9jjidasjp october whisky odiajdq", "English")
+        self.assertEqual(result, False)
+    def test_english_percentage(self):
+        dc = dictionaryChecker.dictionaryChecker()
+        result = dc.confirmlanguage("The password for my computer is tyu456q and the username is admin", "English")
+        self.assertEqual(dc.languagePercentage, 80.0)
