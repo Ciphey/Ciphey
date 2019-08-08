29c430160c4373c3b190260a5415b65550609e10
Brandon <brandonskerritt51@gmail.com>
2019-08-08 14:13:53 +0100

    added basic encoding and hashing functionality
    Former-commit-id: 3c3fd426b6e8b512ea667025614eaab9b3436ceb
    Former-commit-id: 2794bc6a9927d441096cc59a990dc417adfc5e73

---
+++ app/Decryptor/Encoding/base64.py
@@ -0,0 +1,3 @@
+class Base64:
+    def __init__(self, lc):
+        self.lc = lc
---
+++ app/Decryptor/Encoding/encodingParent.py
@@ -0,0 +1,50 @@
+class EncodingParent:
+    def __init__(self, lc):
+        self.lc = lc
+    def decrypt(self, text):
+        self.text = text
+        torun = [self.base64]
+        """
+        ok so I have an array of functions
+        and I want to apply each function to some text
+        (text, function)
+        but the way it works is you apply text to every item in the array (function)
+
+        """
+        from multiprocessing.dummy import Pool as ThreadPool 
+        pool = ThreadPool(4) 
+        results = pool.map(callFunction, torun)
+
+        """
+        Ok so this one will have a list of all functions it can call
+        then it'll map.apply that for threading
+        """
+        pass
+    def callFunction(func):
+        return func(self.text)
+    def base64(self, text):
+        import base64
+        try:
+            result = base64.b64decode(text)
+        except binascii.Error as e:
+            return {"lc": self.lc, "IsPlaintext?": False, "Plaintext": None, "Cipher": None, "Extra Information": None}
+
+        return {"lc": self.lc, "IsPlaintext?": True, "Plaintext": result, "Cipher": "Base64 encoded", "Extra Information": None}
+    def binary(self, text):
+        import binascii
+
+        def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
+            n = int(bits, 2)
+            return int2bytes(n).decode(encoding, errors)
+        
+        def int2bytes(i):
+            hex_string = '%x' % i
+            n = len(hex_string)
+            return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
+
+        try:
+            result = text_from_bits(text)
+        except ValueError as e:
+            return {"lc": self.lc, "IsPlaintext?": False, "Plaintext": None, "Cipher": None, "Extra Information": None}
+        return {"lc": self.lc, "IsPlaintext?": True, "Plaintext": result, "Cipher": "ASCII to Binary encoded", "Extra Information": None}
+        
