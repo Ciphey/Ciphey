a66b4649a7af69ac811cbc85d4a3a5abf13fd38a
brandon <brandonskerritt51@gmail.com>
2019-08-11 00:46:26 +0100

    finished encoding i think

---
+++ app/Decryptor/Encoding/test.py
@@ -0,0 +1,16 @@
+import binascii
+
+def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
+    bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
+    return bits.zfill(8 * ((len(bits) + 7) // 8))
+
+def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
+    n = int(bits, 2)
+    return int2bytes(n).decode(encoding, errors)
+
+def int2bytes(i):
+    hex_string = '%x' % i
+    n = len(hex_string)
+    return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
+print(text_to_bits('hello'))
+print(text_from_bits('110100001100101011011000110110001101111'))
