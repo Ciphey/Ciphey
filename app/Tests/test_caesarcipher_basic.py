e7c111fa6aecfd1fb9932b1d170a216c6efa377b
Brandon <brandonskerritt51@gmail.com>
2020-04-29 01:24:53 +0100

    All tests - FIXED
    !!! :)
    
    Former-commit-id: 4261b3db5a1a60d31194216404732e777bcb79ac [formerly e412517079931a4c39c60734a8fb6ba1577426f1] [formerly 2eb422badf57604c6a227565eebf3dccb9ebbf17] [formerly 70937036df4d661d1b37027784a2cfb80fc294a9]
    Former-commit-id: 697285af563b743933c71a538d2f44501dcf2e0e
    Former-commit-id: 055e5b70af73685fb66e984be5346c5613ec8f95
    Former-commit-id: cc83a48292bfa53109ac977069389033a34a5d44
    Former-commit-id: 59965cfb2d265af3f2a393d7c06a3048703cc497

--- app/Tests/test_caesarcipher_basic.py
+++ app/Tests/test_caesarcipher_basic.py
@@ -3,7 +3,7 @@ import sys
 
 import unittest
 
-from app.Decryptor.basicEncryption.caesar import Caesar
+import app.Decryptor.basicEncryption.caesar
 import app.languageCheckerMod.LanguageChecker
 # python3 -m unittest Tests.testchi_squared
 # python -m unittest discover -s tests
@@ -11,19 +11,18 @@ import app.languageCheckerMod.LanguageChecker
 
 # {"lc": self.lc, "IsPlaintext?": True, "Plaintext": translated, "Cipher": "Caesar"}
 
-
 class TestChi(unittest.TestCase):
     def test_caesar_yes(self):
         """Checks to see if it returns True (it should)"""
         lc = app.languageCheckerMod.LanguageChecker.LanguageChecker()
-        c = Caesar.Caesar(lc)
+        c = app.Decryptor.basicEncryption.caesar.Caesar(lc)
         result = c.decrypt("uryyb zl sngure uryyb zl zbgure naq v ernyyl qb yvxr n tbbq ratyvfu oernxsnfg")
         self.assertEqual(result['IsPlaintext?'], True)
 
     def test_caesar_no(self):
         """Checks to see if it returns True (it should)"""
         lc = app.languageCheckerMod.LanguageChecker.LanguageChecker()
-        c = Caesar.Caesar(lc)
+        c = app.Decryptor.basicEncryption.caesar.Caesar(lc)
         result = c.decrypt("o iozad iikwas")
         self.assertEqual(result['IsPlaintext?'], False)
 
@@ -32,19 +31,19 @@ class TestChi(unittest.TestCase):
         Ok so this returns false becaues caesar only does up to 25, not 26
         so plaintext returns false!"""
         lc = app.languageCheckerMod.LanguageChecker.LanguageChecker()
-        c = Caesar.Caesar(lc)
+        c = app.Decryptor.basicEncryption.caesar.Caesar(lc)
         result = c.decrypt("What about plaintext?")
         self.assertEqual(result['IsPlaintext?'], True)
 
     def test_caesar_english_comparison(self):
         lc = app.languageCheckerMod.LanguageChecker.LanguageChecker()
-        c = Caesar.Caesar(lc)
+        c = app.Decryptor.basicEncryption.caesar.Caesar(lc)
         result = c.decrypt("Pm ol ohk hufaopun jvumpkluaphs av zhf, ol dyval pa pu jpwoly, aoha pz, if zv johunpun aol vykly vm aol slaalyz vm aol hswohila, aoha uva h dvyk jvbsk il thkl vba.")
         self.assertEqual(result['IsPlaintext?'], True)
 
     def test_caesar_english_comparison_yeet(self):
         lc = app.languageCheckerMod.LanguageChecker.LanguageChecker()
-        c = Caesar.Caesar(lc)
+        c = app.Decryptor.basicEncryption.caesar.Caesar(lc)
         result = c.decrypt("Onrs hr mnv knbjdc enq fnnc. Sgzmj xnt zkk enq ozqshbhozshmf, zmc okdzrd bnmshmtd sn unsd enq sgd itcfdldms xnt zfqdd vhsg. Zr nsgdq trdqr gzud onhmsdc nts, sgdqd hr zcchshnmzk hmenqlzshnm sgzs NO gzr kdes nts ne sgdhq nqhfhmzk onrs sgzs sgdx gzud zccdc hm sgdhq bnlldmsr, rn okdzrd qdzc etqsgdq sn fds sgd etkk rbnod ne sgd rhstzshnm.")
         self.assertEqual(result['Plaintext'], "Post is now locked for good. Thank you all for participating, and please continue to vote for the judgement you agree with. As other users have pointed out, there is additional information that OP has left out of their original post that they have added in their comments, so please read further to get the full scope of the situation.".lower())
 
@@ -53,6 +52,6 @@ class TestChi(unittest.TestCase):
         Ok so this returns false becaues caesar only does up to 25, not 26
         so plaintext returns false!"""
         lc = app.languageCheckerMod.LanguageChecker.LanguageChecker()
-        c = Caesar.Caesar(lc)
+        c = app.Decryptor.basicEncryption.caesar.Caesar(lc)
         result = c.decrypt("?")
         self.assertEqual(result['IsPlaintext?'], False)
