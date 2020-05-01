8711bfb12c3a7fefd2e8b0a488fda1444e2bb1d2
Brandon <brandonskerritt51@gmail.com>
2020-05-01 21:54:33 +0100

    Fixing PyPi
    Former-commit-id: e93c4fd816a858bbb5de77b2369c6cb0e0dd1797 [formerly 21038eda3a210ac8639dbad17665ecc919bededa] [formerly f90f778e14646ad220684d583bb720f5ba3ec480] [formerly 5c4269da54036388da9ffa51ab73ec49bc39b8a8]
    Former-commit-id: f668e189a8cf89617dcec4080da16849e5552f17
    Former-commit-id: 3ff858731d1cbe32909271fe5b2ae29ac3106cd4
    Former-commit-id: 9ccde968af729ed12433576b2fa6811a11530127
    Former-commit-id: 5fb1e3f9df158a6de682623892372010bf655844

--- setup.py
+++ setup.py
@@ -1,42 +1,13 @@
-import pathlib
-from setuptools import setup, find_packages
-import io
-import os
+from setuptools import setup
 
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "README.MD").read_text()
-long_description = "An automated decryption tool using machine learning"
-DESCRIPTION = "An automated decryption tool using machine learning"
-
-# get list of required installs
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
-# This call to setup() does all the work
 setup(
-    name="Ciphey",
-    version="3.0.2",
-    description="Automated decryption tool using machine learning & common sense",
-    long_description=README,
-    long_description_content_type="text/markdown",
-    url="https://github.com/brandonskerritt/ciphey",
-    author="Brandon Skerritt",
-    author_email="brandon@skerritt.blog",
-    license="MIT",
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-    ],
-    packages=find_packages("Ciphey", exclude=("tests",)),
-    include_package_data=True,
-    install_requires=required,
-    entry_points={
-        "console_scripts": [
-            "ciphey=app.__main__:main",
-        ]
-    },
+    name='Ciphey',
+    version='3.0.5',
+    packages=['app', 'app.Decryptor', 'app.Decryptor.Encoding', 'app.Decryptor.basicEncryption', 'app.neuralNetworkMod',
+              'app.languageCheckerMod'],
+    url='https://github.com/brandonskerritt/ciphey',
+    license='MIT',
+    author='Brandon Skerritt',
+    author_email='brandon@skerritt.blog',
+    description='Automated decryption tool using machine learning'
 )
