e10041941237f2983cc6ad1a0600ee7543ab451c
Brandon <brandonskerritt51@gmail.com>
2020-05-02 17:07:12 +0100

    Changed `app` to `ciphey`
    Former-commit-id: 6ff36b9824ce1163ffd9044c348f9e7dd352444f [formerly 3c6f2bf5f2fd0558c7757c785007b7482931d674] [formerly 181139f5ee7a90b93ac612103ead8f8bc794d01f] [formerly f74667c628c78c9bbebad3aed031bce8aa176f67]
    Former-commit-id: 7dca738919d58bc4ee5922dc9c3c3eecb50bed1e
    Former-commit-id: 828fbc7e903f00cc7178a243858e55e5c9c993d7
    Former-commit-id: a8e4d6b3f6da0e14ea21fb5e2bd7fe799eb4bc72
    Former-commit-id: 543ed0ee01c1be24f36cc8195636eeec49557f56

---
+++ path.py
@@ -0,0 +1,88 @@
+"""Adds a directory to sys.path, permanently.
+
+"""
+
+import sys
+import os
+import site
+
+HEADER = """\
+# .pth file created by the pypath script.
+# edit at your own risk
+"""
+
+SITEDIR = site.getusersitepackages()
+PATH = os.path.join(SITEDIR, "pypath.pth")
+
+if not os.path.exists(SITEDIR):
+    os.mkdirs(SITEDIR)
+
+
+def add(path, pth_file=None):
+    path = path.strip()
+    pth_file = pth_file or PATH
+    if not os.path.exists(path):
+        raise ValueError("doesn't exist: {}".format(path))
+    with open(pth_file, "r") as f:
+        for line in f:
+            if line.strip(os.linesep) == path:
+                # already added
+                return
+        empty = not f.tell()
+    with open(pth_file, "a") as f:
+        if empty:
+            f.write(HEADER)
+        f.write(os.linesep + path)
+
+
+def remove(path, pth_file=None):
+    pth_file = pth_file or PATH
+    try:
+        with open(pth_file, "r") as f:
+            lines = f.read().splitlines()[2:]
+    except IOError:
+        raise ValueError("not found: {}".format(path))
+
+    try:
+        lines.remove(path)
+    except ValueError:
+        raise ValueError("not found: {}".format(path))
+
+    lines.insert(0, HEADER)
+    with open(pth_file, "w") as f:
+        f.write(os.linesep.join(lines))
+
+
+def show(pth_file=None):
+    pth_file = pth_file or PATH
+    print("current paths ({}):".format(pth_file))
+    try:
+        f = open(pth_file, "r")
+    except IOError:
+        return
+    with f:
+        lines = f.read()
+        if not lines.startswith(HEADER):
+            raise ValueError(".pth file is invalid: {}".format(pth_file))
+        for line in lines.splitlines()[2:]:
+            if not line.strip():
+                continue
+            print(line)
+
+
+if __name__ == "__main__":
+    import argparse
+    EPILOG = "If no args are passed, the current contents are shown."
+    parser = argparse.ArgumentParser(epilog=EPILOG)
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-a", "--add", metavar="PATH")
+    group.add_argument("-r", "--remove", metavar="PATH")
+    parser.add_argument("-f", "--file", metavar="<path to .pth file>")
+    args = parser.parse_args()
+
+    if args.add:
+        add(args.add, args.file)
+    elif args.remove:
+        remove(args.remove, args.file)
+    else:
+        show(args.file)
