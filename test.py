f712ee4076226c2b70fd49d7c7ce9be599d44908
Brandon <brandonskerritt51@gmail.com>
2020-05-09 19:13:33 +0100

    Think i fixed dict sorting in test.py
    Former-commit-id: d6390f33b7a86b9b6d655bc6ad465487f1d41d4f [formerly 6da0047157bdbca6efea132b9d1544909461cd8a] [formerly 6132a4621c5a01340464dee7cf0789163b8ed2dd] [formerly b8c634bfcd897962dd46e2fc5c15628f10fe2905]
    Former-commit-id: 522b4b0c2b61132c2550b21320d1c3d0141125c6
    Former-commit-id: 551f8e126894b11947301ab06f171112a35f2a54
    Former-commit-id: 1e0db06e2ac1204ffd8b8951b75cb5a92ba2c669
    Former-commit-id: 5e0339a351157d8e8041b780697ab202e56bb925

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
+from collections import OrderedDict
 def sortProbTable(probTable):
     """Sorts the probabiltiy table"""
     # for each object: prob table in dictionary
     maxOverall = 0
     maxDictPair = {}
     highestKey = None
-    print(probTable)
+    
+    # sorts the prob table before we find max, and converts it to order dicts
     for key, value in probTable.items():
-        print(key, value)
+        probTable[key] = newSort(value)
+
+    
+    # gets maximum key then sets it to the front
+    for key, value in probTable.items():
         maxLocal = 0
         # for each item in that table
         print(value)
@@ -32,5 +38,27 @@ def sortProbTable(probTable):
     print(d)
     return d
 
-sortProbTable({"hash parent": {'md5': 0.01, 'sha1': 0.01, 'sha256': 0.01, 'sha512': 0.01}, "basic parent": {'caesar': 0.1266546}, 
-'plaintext': {'plaintext': 0.92581327}, "encoding parent": {'ascii': 0.01, 'base64': 0.22110419, 'binary': 0.11, 'hexadecimal': 0.024687605, 'morse': 0.01, 'reverse': 0.01}})
+def sortDictionary(dictionary):
+    """Sorts a dictionary"""
+    
+    return dict(OrderedDict(sorted(dictionary.items())))
+
+def newSort(newDict):
+    # gets the key of the dictionary
+    # keyDict = list(newDict.keys())[0]
+    # gets the items of that key (which is a dictionary)
+    d = dict(newDict)
+    
+    
+    
+    # (f"d is {d}")
+    print("\n")
+    sortedI = OrderedDict(sorted(d.items(), key=lambda x: x[1], reverse=True))
+    # sortedI = sortDictionary(x)
+    print(f"sortedI is {sortedI}")
+    return sortedI
+#sortProbTable({"hash parent": {'md5': 0.01, 'sha1': 0.01, 'sha256': 0.01, 'sha512': 0.01}, "basic parent": {'caesar': 0.1266546}, 
+#'plaintext': {'plaintext': 0.92581327}, "encoding parent": {'ascii': 0.01, 'base64': 0.22110419, 'binary': 0.11, 'hexadecimal': 0.024687605, 'morse': 0.01, 'reverse': 0.01}})
+sortProbTable({"hash parent": {'md5': 0.5, 'sha1': 0.01, 'sha256': 0.01, 'sha512': 0.01}, "basic parent": {'caesar': 0.1}, 
+'plaintext': {'plaintext': 0.1}, "encoding parent": {'ascii': 0.1, 'base64': 0.2, 'binary': 0.3, 'hexadecimal': 0.4, 'morse': 0.5, 'reverse': 0.6}})
+#print(newSort({"encoding parent": {'ascii': 0.01, 'base64': 0.22110419, 'binary': 0.11, 'hexadecimal': 0.024687605, 'morse': 0.01, 'reverse': 0.01}}))
