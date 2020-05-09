c8fbe5e19dc41e2b7c98177fa1eebc6f45c9f21d
Brandon <brandonskerritt51@gmail.com>
2020-05-09 20:19:54 +0100

    Bug fixes
     [formerly 3629f28261cd78d011207af7424221f6fecd0d52] [formerly 429800a7f7bcf963502852522ff64bfc0f0971ab] [formerly 5838b6465e6c74849d6bd345bf1bb5b488d3eb41]

--- ciphey/Decryptor/Encoding/encodingParent.py
+++ ciphey/Decryptor/Encoding/encodingParent.py
@@ -34,6 +34,7 @@ class EncodingParent:
 
         for answer in answers:
             # adds the LC objects together
+            print(f"\n\n The answer object is {answer}\n\n")
             self.lc = self.lc + answer["lc"]
             if answer["IsPlaintext?"]:
                 return answer
