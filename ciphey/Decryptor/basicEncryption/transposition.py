2c63ea385ae07c147ed99163495fd534d8f819a7
Brandon <brandonskerritt51@gmail.com>
2020-05-16 01:36:08 +0100

    I have transposition working on its own, now integration
    Former-commit-id: 696141f9276eb518fb3aa273d9913c359290fe23 [formerly d371261cf86a9b5fca9db5b942a5ec8ab7b7be2e] [formerly 4645cfad04d86cfd26e9dffcf3c060f7f004f255] [formerly 2a4e557d0c044d59da73339ab1ba46d1dcc30864]
    Former-commit-id: 828c17c6db91f93dff379c646c30086455ac6ca9
    Former-commit-id: 61db613473722b6652a23f9d12be8c757ae76ed7
    Former-commit-id: ff339d8474eb8c1803721569c13904de205d4f27
    Former-commit-id: cff9421d6ab4ba277b075ae733174da9a13dfc2c

--- ciphey/Decryptor/basicEncryption/transposition.py
+++ ciphey/Decryptor/basicEncryption/transposition.py
@@ -40,19 +40,23 @@ class Transposition:
         hackedMessage = self.hackTransposition(myMessage)
 
     def hackTransposition(self, message):
+        message=="""Cb b rssti aieih rooaopbrtnsceee er es no npfgcwu  plri ch nitaalr eiuengiteehb(e1  hilincegeoamn fubehgtarndcstudmd nM eu eacBoltaeteeoinebcdkyremdteghn.aa2r81a condari fmps" tad   l t oisn sit u1rnd stara nvhn fsedbh ee,n  e necrg6  8nmisv l nc muiftegiitm tutmg cm shSs9fcie ebintcaets h  aihda cctrhe ele 1O7 aaoem waoaatdahretnhechaopnooeapece9etfncdbgsoeb uuteitgna.rteoh add e,D7c1Etnpneehtn beete" evecoal lsfmcrl iu1cifgo ai. sl1rchdnheev sh meBd ies e9t)nh,htcnoecplrrh ,ide hmtlme. pheaLem,toeinfgn t e9yce da' eN eMp a ffn Fc1o ge eohg dere.eec s nfap yox hla yon. lnrnsreaBoa t,e eitsw il ulpbdofgBRe bwlmprraio po  droB wtinue r Pieno nc ayieeto'lulcih sfnc  ownaSserbereiaSm-eaiah, nnrttgcC  maciiritvledastinideI  nn rms iehn tsigaBmuoetcetias rn"""
         # we could probably multi thread this
         for key in range(1, len(message)):
             decryptedText = self.decryptMessage(key, message)
+            if key == 10:
+                print(decryptedText)
+                exit(1)
             # if decrypted text is english, return true
             if self.lc.checkLanguage(decryptedText):
                 print("found decrypted text")
-                return {
+                """return {
                     "lc": self.lc,
                     "IsPlaintext?": True,
                     "Plaintext": decryptedText,
                     "Cipher": "Transposition",
                     "Extra Information": f"The key is {key}",
-                }
+                }"""
         # after all keys, return false
         print("no decrypted text")
         return {
