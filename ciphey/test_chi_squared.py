88fb90187b1ed73363de3fc9e832b237312f949b
Brandon <brandonskerritt51@gmail.com>
2020-05-17 02:21:53 +0100

    Fixing LC

--- ciphey/test_chi_squared.py
+++ ciphey/test_chi_squared.py
@@ -284,6 +284,17 @@ class testChi(unittest.TestCase):
         )
         self.assertEqual(result, False)
 
+        def test_english_babbage(self):
+            self.chi = chiSquared()
+            """
+            Tests to see whether a sentene is classified as English or not
+            Returns False because exclamation marks aren't english
+            """
+            result = self.chi.checkChi(
+                """Charles Babbage, FRS (26 December 1791 - 18 October 1871) was an English mathematician, philosopher, inventor and mechanical engineer who originated the concept of a programmable computer. Considered a father of the computer, Babbage is credited with inventing the first mechanical computer that eventually led to more complex designs. Parts of his uncompleted mechanisms are on display in the London Science Museum. In 1991, a perfectly functioning difference engine was constructed from Babbage's original plans. Built to tolerances achievable in the 19th century, the success of the finished engine indicated that Babbage's machine would have worked. Nine years later, the Science Museum completed the printer Babbage had designed for the difference engine."""
+            )
+            self.assertEqual(result, True)
+
     def test_my_chi(self):
         self.chi = chiSquared()
         result = self.chi.myChi(
