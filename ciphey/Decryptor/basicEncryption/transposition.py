8150741b6a0c16ab0a6b152401e91e0a72b6855f
Brandon <brandonskerritt51@gmail.com>
2020-05-20 12:16:42 +0100

    Added loggingGuru
    Former-commit-id: 9f9c1edfb067b32009ce1f9ea25bd45bcf0f85e0 [formerly 88a6ed319274d59210ef999d5a49302f3c40fe7c] [formerly 68875402eef8f1b849a9ea2205114726d8280a33] [formerly e1f24aabad0d9fa5116174df3815d089cabd4c93]
    Former-commit-id: 163ad67176d04eabe5c85bba55b19f65ba063fd1
    Former-commit-id: a27c41195ea8f25234d9d908f87016f7f2918980
    Former-commit-id: 8785daf583cb818662dc69e587b82c59cbb62251
    Former-commit-id: de3bace0970cc411267d3f4c294e107f87a2eefc

--- ciphey/Decryptor/basicEncryption/transposition.py
+++ ciphey/Decryptor/basicEncryption/transposition.py
@@ -87,17 +87,3 @@ class Transposition:
         return "".join(plaintext)
 
 
-def main():
-    # You might want to copy & paste this text from the source code at
-    # http://invpy.com/transpositionHacker.py
-    myMessage = """Cb b rssti aieih rooaopbrtnsceee er es no npfgcwu  plri ch nitaalr eiuengiteehb(e1  hilincegeoamn fubehgtarndcstudmd nM eu eacBoltaeteeoinebcdkyremdteghn.aa2r81a condari fmps" tad   l t oisn sit u1rnd stara nvhn fsedbh ee,n  e necrg6  8nmisv l nc muiftegiitm tutmg cm shSs9fcie ebintcaets h  aihda cctrhe ele 1O7 aaoem waoaatdahretnhechaopnooeapece9etfncdbgsoeb uuteitgna.rteoh add e,D7c1Etnpneehtn beete" evecoal lsfmcrl iu1cifgo ai. sl1rchdnheev sh meBd ies e9t)nh,htcnoecplrrh ,ide hmtlme. pheaLem,toeinfgn t e9yce da' eN eMp a ffn Fc1o ge eohg dere.eec s nfap yox hla yon. lnrnsreaBoa t,e eitsw il ulpbdofgBRe bwlmprraio po  droB wtinue r Pieno nc ayieeto'lulcih sfnc  ownaSserbereiaSm-eaiah, nnrttgcC  maciiritvledastinideI  nn rms iehn tsigaBmuoetcetias rn"""
-    x = Transposition("hello")
-    hackedMessage = x.decrypt(myMessage)
-
-    if hackedMessage == None:
-        print("Failed to hack encryption.")
-    else:
-
-
-if __name__ == "__main__":
-    main()
