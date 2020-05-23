0e147e1489a2066444a81df8654a846eb0d21b11
Brandon <brandonskerritt51@gmail.com>
2020-05-24 00:54:11 +0100

    Transposition now works
     [formerly 97359b2e0dc3b81407eb6d598dbf66ceb7066b30] [formerly d2a1b912761904ef327a49a7086ec90988e11d0a] [formerly 3c154eed05b2c47dccf928b380bafa749af57ce4]

--- ciphey/Decryptor/basicEncryption/transposition.py
+++ ciphey/Decryptor/basicEncryption/transposition.py
@@ -24,8 +24,6 @@ class Transposition:
 
     def decrypt(self, text):
         # Brute-force by looping through every possible key.
-        text = """Cb b rssti aieih rooaopbrtnsceee er es no npfgcwu  plri ch nitaalr eiuengiteehb(e1  hilincegeoamn fubehgtarndcstudmd nM eu eacBoltaeteeoinebcdkyremdteghn.aa2r81a condari fmps" tad   l t oisn sit u1rnd stara nvhn fsedbh ee,n  e necrg6  8nmisv l nc muiftegiitm tutmg cm shSs9fcie ebintcaets h  aihda cctrhe ele 1O7 aaoem waoaatdahretnhechaopnooeapece9etfncdbgsoeb uuteitgna.rteoh add e,D7c1Etnpneehtn beete" evecoal lsfmcrl iu1cifgo ai. sl1rchdnheev sh meBd ies e9t)nh,htcnoecplrrh ,ide hmtlme. pheaLem,toeinfgn t e9yce da' eN eMp a ffn Fc1o ge eohg dere.eec s nfap yox hla yon. lnrnsreaBoa t,e eitsw il ulpbdofgBRe bwlmprraio po  droB wtinue r Pieno nc ayieeto'lulcih sfnc  ownaSserbereiaSm-eaiah, nnrttgcC  maciiritvledastinideI  nn rms iehn tsigaBmuoetcetias rn"""
-
         decryptedText = self.hackTransposition(text)
         return decryptedText
 
@@ -37,8 +35,6 @@ class Transposition:
             decryptedText = self.decryptMessage(key, message)
             # if decrypted english is found, return them
             result = self.lc.checkLanguage(decryptedText)
-            if key == 6:
-                logger.debug(f"KEY 6 HAS BEEN REACHED")
             if result:
                 logger.debug("transposition returns true")
                 return {
