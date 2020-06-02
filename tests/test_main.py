dfec4cc5c2ee22d29eda73db378dcc497a577004
Brandon <brandonskerritt51@gmail.com>
2020-06-03 00:04:01 +0100

    Pytest now works with main overall, but doesnt use internal data packet
    Former-commit-id: 304a03c7590f231d7be9568e1608f20ed48951e0
    Former-commit-id: 9e04dc528813d361fda7d0fc8a224c793a84ce86

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,5 +2,6 @@ from Ciphey.ciphey.__main__ import main
 
 
 def test_argument_grep_true():
-    result = main("hello")
+    result = main(text="hello")
+    print(result)
     assert result["Plaintext"] == "hello"
