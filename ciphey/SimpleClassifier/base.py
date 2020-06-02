2044a7ac92c1b212c602f4643b130e5baf000ff8
Cyclic3 <cyclic3.git@gmail.com>
2020-06-02 16:20:18 +0100

    That should do it for the simple classifier

--- ciphey/SimpleClassifier/base.py
+++ ciphey/SimpleClassifier/base.py
@@ -1,3 +1,11 @@
-class Classifier():
-  def name() -> str: pass
-  def isImpossible(data: str) -> bool: pass
+class ClassifierBase():
+    def name() -> str: pass
+
+class Classifier(ClassifierBase):
+    def isImpossible(data: str) -> bool: pass
+
+class EncodingClassifier(ClassifierBase):
+    def classify(data: str) -> set: pass
+
+class LengthInformedClassifier(ClassifierBase):
+    def isImpossible(data: str, lengths: set) -> bool: pass
