1c4298fd50bb58dbf70262a554dc5339910c7ff2
Cyclic3 <cyclic3.git@gmail.com>
2020-06-02 15:24:30 +0100

    Added binary classifier
    Former-commit-id: f11c7316846d0e148edd7c440c0d821234b34193
    Former-commit-id: d49e70cedfca017991c66697e17abfc29e2b87e4

--- ciphey/SimpleClassifier/encoding.py
+++ ciphey/SimpleClassifier/encoding.py
@@ -7,15 +7,20 @@ def checkCharsIn(data : str, charset : set):
 
 
 class HexClassifier(SimpleClassifier.base.Classifier):
-    def name(): return "hex"
+    def name(): return "base16"
     def isImpossible(data: str):
         # Skip 0x
         if (len(data) >= 2 and data[0] == '0' and (data[1] == 'x' or data[1] == 'X')):
           data = data[2:]
         return not checkCharsIn(data, set(cipheydists.get_charset("encodings")["hex"]))
 
-
 class Base64Classifier(SimpleClassifier.base.Classifier):
     def name(): return "base64"
     def isImpossible(data: str):
         return not checkCharsIn(data, set(cipheydists.get_charset("encodings")["base64"]))
+
+class BinaryClassifier(SimpleClassifier.base.Classifier):
+    def name(): return "base2"
+    def isImpossible(data: str):
+        return not checkCharsIn(data, set(0,1))
+
