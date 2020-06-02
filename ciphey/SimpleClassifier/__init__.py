8c775fd97371bfffa5738708d965dc6f6d1dea77
Cyclic3 <cyclic3.git@gmail.com>
2020-06-02 16:20:18 +0100

    That should do it for the simple classifier
    Former-commit-id: f82909fcc1c7a03aa8e0c79a026f02c551e00df1
    Former-commit-id: eca5c4fba4eab0a1915e0e84ce293a7721ee27cd

--- ciphey/SimpleClassifier/__init__.py
+++ ciphey/SimpleClassifier/__init__.py
@@ -1,8 +1,25 @@
-from SimpleClassifier.encoding import *
+from SimpleClassifier.encoding import all_encoding_classifiers, all_encodings
+from SimpleClassifier.hashes import all_hash_classifiers, all_hashes
 
-def filterOut(data:str) -> set:
-    ret = set()
-    for c in [HexClassifier, Base64Classifier, BinaryClassifier]:
-        if c.isImpossible(data):
-            ret.add(c.name())
+class filterResults():
+    failed_encodings = set()
+    failed_hashes = set()
+    lengths = set()
+
+def filterOut(data:str) -> filterResults:
+    ret = filterResults()
+    ret.lengths.add(len(data))
+    # First, gather the encodings, so that we can get the lengths
+    for encoding in all_encoding_classifiers:
+        res = encoding.classify(data)
+        if res:
+            ret.lengths |= res
+        else:
+            ret.failed_encodings.add(encoding.name())
+
+    # Now we can iterate over the rest
+    for hash in all_hash_classifiers:
+        if hash.isImpossible(data, ret.lengths):
+            ret.failed_hashes.add(hash.name())
+
     return ret
