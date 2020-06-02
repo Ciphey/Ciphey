174428e3f764999b6c5fbe2243c55b26e292f2fa
Cyclic3 <cyclic3.git@gmail.com>
2020-06-02 15:21:35 +0100

    First impl of simple filter
    Former-commit-id: e29ab0ea75ad938d81e8b0a9f55b68cb45051a62 [formerly dde72300a9267ac4a41c889c6f0b388bd3a71005] [formerly b9b1a84e761d7df53408805cbcf7d2d6c94f2dfc] [formerly 6520e915ea843eb441b8a746969e0a92ccb798f7]
    Former-commit-id: 29d4379f5e5c274e2a7236bc2ffc698679fe7dd6
    Former-commit-id: dba9bfd1b53764774bd050757457f374123891cf
    Former-commit-id: 285fece9a419f7008516af2c728b8d086fc21a5a
    Former-commit-id: 649569490828d045e8865489ddc665113637d3c6

---
+++ ciphey/SimpleClassifier/encoding.py
@@ -0,0 +1,21 @@
+import SimpleClassifier.base
+
+import cipheydists
+
+def checkCharsIn(data : str, charset : set):
+    return all(char in charset for char in data)
+
+
+class HexClassifier(SimpleClassifier.base.Classifier):
+    def name(): return "hex"
+    def isImpossible(data: str):
+        # Skip 0x
+        if (len(data) >= 2 and data[0] == '0' and (data[1] == 'x' or data[1] == 'X')):
+          data = data[2:]
+        return not checkCharsIn(data, set(cipheydists.get_charset("encodings")["hex"]))
+
+
+class Base64Classifier(SimpleClassifier.base.Classifier):
+    def name(): return "base64"
+    def isImpossible(data: str):
+        return not checkCharsIn(data, set(cipheydists.get_charset("encodings")["base64"]))
