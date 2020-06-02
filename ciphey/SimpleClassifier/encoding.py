b976bd1f580474b623e5ba1592ecc8e30549cf55
Cyclic3 <cyclic3.git@gmail.com>
2020-06-02 16:20:18 +0100

    That should do it for the simple classifier
    Former-commit-id: 425d6c20685cae1ad5f2acf3162ae390e03e7c71 [formerly 640a1a01ecab944e3ff3e0ed6f15a563c136d471] [formerly 65ce7d1a500b7afb9e4949ddd5c42551aa5b11ea] [formerly 2044a7ac92c1b212c602f4643b130e5baf000ff8]
    Former-commit-id: eca5c4fba4eab0a1915e0e84ce293a7721ee27cd
    Former-commit-id: e382336e633e60a1299eec411b90ad1ecb1e1f0a
    Former-commit-id: 155385a732bb56519ec8747323e616ae8c3ee67d
    Former-commit-id: ec0177ee6ed21bec882912ba75cd2b0b253f35a1

--- ciphey/SimpleClassifier/encoding.py
+++ ciphey/SimpleClassifier/encoding.py
@@ -6,21 +6,37 @@ def checkCharsIn(data : str, charset : set):
     return all(char in charset for char in data)
 
 
-class HexClassifier(SimpleClassifier.base.Classifier):
+class HexClassifier(SimpleClassifier.base.EncodingClassifier):
     def name(): return "base16"
-    def isImpossible(data: str):
+    def classify(data: str):
         # Skip 0x
         if (len(data) >= 2 and data[0] == '0' and (data[1] == 'x' or data[1] == 'X')):
-          data = data[2:]
-        return not checkCharsIn(data, set(cipheydists.get_charset("encodings")["hex"]))
+            data = data[2:]
+        if not checkCharsIn(data, set(cipheydists.get_charset("encodings")["hex"])):
+            return set()
+        return {-(-len(data) // 2)}
 
 class Base64Classifier(SimpleClassifier.base.Classifier):
     def name(): return "base64"
-    def isImpossible(data: str):
-        return not checkCharsIn(data, set(cipheydists.get_charset("encodings")["base64"]))
+    def classify(data: str):
+        if not checkCharsIn(data, set(cipheydists.get_charset("encodings")["base64"])):
+            return set()
+        return {-(-len(data) // 3) * 4}
 
 class BinaryClassifier(SimpleClassifier.base.Classifier):
     def name(): return "base2"
-    def isImpossible(data: str):
-        return not checkCharsIn(data, set(0,1))
+    def classify(data: str):
+        if (len(data) >= 2 and data[0] == '0' and data[1] == 'b'):
+          data = data[2:]
+        if not checkCharsIn(data, {0,1}):
+            return set()
+        return {-(-len(data) // 2)}
 
+class DecimalClassifier(SimpleClassifier.base.Classifier):
+    def name(): return "base10"
+    def classify(data: str):
+        try: return {-(-int(data).bit_length() // 8)}
+        except: return set()
+
+all_encoding_classifiers = [HexClassifier, Base64Classifier, BinaryClassifier, DecimalClassifier]
+all_encodings = [i.name() for i in all_encoding_classifiers]
