43b31c95f7e40859f72c6469d0a7437d57e867e3
Cyclic3 <cyclic3.git@gmail.com>
2020-06-02 15:21:35 +0100

    First impl of simple filter
     [formerly dde72300a9267ac4a41c889c6f0b388bd3a71005] [formerly a1754fb1770031a3d9add0340bbb3127c87e7e54] [formerly 1efd115cbf51a4d4feb9302529a15c6ab32be571]

---
+++ ciphey/SimpleClassifier/__init__.py
@@ -0,0 +1,8 @@
+from SimpleClassifier.encoding import *
+
+def filterOut(data:str) -> set:
+    ret = set()
+    for c in [HexClassifier, Base64Classifier]:
+        if c.isImpossible(data):
+            ret.add(c.name())
+    return ret
