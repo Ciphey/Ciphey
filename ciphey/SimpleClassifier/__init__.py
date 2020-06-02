69dce1c862381717e9262b99d606bdba75e43b74
Cyclic3 <cyclic3.git@gmail.com>
2020-06-02 15:24:30 +0100

    Added binary classifier

--- ciphey/SimpleClassifier/__init__.py
+++ ciphey/SimpleClassifier/__init__.py
@@ -2,7 +2,7 @@ from SimpleClassifier.encoding import *
 
 def filterOut(data:str) -> set:
     ret = set()
-    for c in [HexClassifier, Base64Classifier]:
+    for c in [HexClassifier, Base64Classifier, BinaryClassifier]:
         if c.isImpossible(data):
             ret.add(c.name())
     return ret
