a0ef541ef9bc567c0590fdc5bbbc5c9ed9cfdffd
Brandon <brandonskerritt51@gmail.com>
2020-06-04 00:43:19 +0100

    Fixing noxfile

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,9 @@ The file for Nox
 import nox
 
 
+
 @nox.session(python=["3.8", "3.7", "3.6"])
 def tests(session):
     session.run("poetry", "install", external=True)
-    session.run("pyhon3 -m pytest", "--cov")
+    session.run("pytest") # , "--cov" 
+
