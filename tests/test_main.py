03c8b9cea6bb6e0cdc82c6600b4080dd3e4408b0
Brandon <brandonskerritt51@gmail.com>
2020-06-04 00:17:48 +0100

    Added Nox to github actions

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,5 +3,15 @@ from Ciphey.ciphey.__main__ import main
 
 def test_argument_grep_true():
     result = main(text="It was the best of times, it was the worst of times")
-    print(result)
     assert result == "It was the best of times, it was the worst of times"
+
+
+def test_main_base64_true():
+    result = main(
+        text="SXQgd2FzIHRoZSBiZXN0IG9mIHRpbWVzLCBpdCB3YXMgdGhlIHdvcnN0IG9mIHRpbWVzLiBUaGVyZSBpcyBvbmx5IHNvIG11Y2ggcm9hZCBpbiBEb3ZlciBvbmUgY2FuIGxheS4=",
+        greppable=True,
+    )
+    assert (
+        result['Plaintext']
+        == "It was the best of times, it was the worst of times. There is only so much road in Dover one can lay."
+    )
