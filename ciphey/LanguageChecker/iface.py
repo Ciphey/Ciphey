3e83bc40d39284e2212aafb62e8bc7ce19e6bfb6
Cyclic3 <cyclic3.git@gmail.com>
2020-06-04 18:08:05 +0100

    Refactored Brandon checker
    Former-commit-id: 2056b26a7979cd6d604b5bf9963a64a8f6f729fa
    Former-commit-id: 40f27d8856f5ff3ab102fb3f22289df6faa62757

---
+++ ciphey/LanguageChecker/iface.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+
+class LanguageChecker(ABC):
+    @staticmethod
+    @abstractmethod
+    def getArgs(**kwargs) -> dict:
+        """The returned dictionary must be of the format:
+            {<name:string>: {"req": <required:bool>, "desc": <description:string>}, ...}
+        """
+        pass
+
+    @abstractmethod
+    def checkLanguage(self, text: str) -> bool: pass
+
+    @abstractmethod
+    def __init__(self, config, params): pass
