f8f0e9e1e851695ec5db8930652674c3a0ff4da2
Brandon <brandonskerritt51@gmail.com>
2020-06-04 12:05:08 +0100

    Added install_with_constraints to nox

--- noxfile.py
+++ noxfile.py
@@ -34,12 +34,6 @@ def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> Non
         session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
-@nox.session(python=["3.8", "3.7", "3.6"])
-def tests(session):
-    session.run("poetry", "install", external=True)
-    session.run("pytest")  # , "--cov"
-
-
 # noxfile.py
 @nox.session(python="3.8")
 def black(session):
@@ -60,5 +54,14 @@ def safety(session):
             f"--output={requirements.name}",
             external=True,
         )
-        session.install("safety")
+        install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+
+@nox.session(python=["3.8", "3.7"])
+def tests(session):
+    args = session.posargs or ["--cov", "-m", "not e2e"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov" 
+    )
+    session.run("pytest", *args)
