c6d822c29ca6090ef9f6e508af7bc47b2a5cb084
Brandon <brandonskerritt51@gmail.com>
2020-06-04 14:19:02 +0100

    Added CODECOV to readme and Nox

--- noxfile.py
+++ noxfile.py
@@ -34,12 +34,6 @@ def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> Non
         session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
-@nox.session(python=["3.8", "3.7", "3.6"])
-def tests(session):
-    session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov=ciphey")
-
-
 # noxfile.py
 @nox.session(python="3.8")
 def black(session):
@@ -62,3 +56,17 @@ def safety(session):
         )
         install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+
+
+@nox.session(python="3.8")
+def coverage(session: Session) -> None:
+    """Upload coverage data."""
+    install_with_constraints(session, "coverage[toml]", "codecov")
+    session.run("coverage", "xml", "--fail-under=0")
+    session.run("codecov", *session.posargs)
+
+
+@nox.session(python=["3.8", "3.7", "3.6"])
+def tests(session):
+    session.run("poetry", "install", external=True)
+    session.run("pytest", "--cov=ciphey")
