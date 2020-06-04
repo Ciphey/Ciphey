52dd15949df146d1c8f59caf89e4aebe0ba5881d
Brandon <brandonskerritt51@gmail.com>
2020-06-04 20:14:40 +0100

    Fixing documentation

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Ciphey'
-copyright = '2020, Brandon S & Cyclic3'
-author = 'Brandon S & Cyclic3'
+project = "Ciphey"
+copyright = "2020, Brandon S & Cyclic3"
+author = "Brandon S & Cyclic3"
 
 # The full version, including alpha/beta/rc tags
-release = '4.1.0'
+release = "4.1.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,15 +31,17 @@ release = '4.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
 ]
-
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,9 +49,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
