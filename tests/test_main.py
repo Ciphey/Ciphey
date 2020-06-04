c8b9847c37912221bc6bb7317582a9210c8f150e
Brandon <brandonskerritt51@gmail.com>
2020-06-04 12:00:56 +0100

    Nox now has Black + Saftey

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,6 +12,6 @@ def test_main_base64_true():
         greppable=True,
     )
     assert (
-        result['Plaintext']
+        result["Plaintext"]
         == "It was the best of times, it was the worst of times. There is only so much road in Dover one can lay."
     )
