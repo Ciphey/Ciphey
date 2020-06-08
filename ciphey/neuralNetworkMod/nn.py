17168b2b3a9fdd9cb1b26060c3e91a3351dc5837
Brandon <brandonskerritt51@gmail.com>
2020-06-08 14:21:59 +0100

    Auto stash before checking out "HEAD"
    Former-commit-id: 3954fe8e949332c4e5da0af0a71c1b3e6bac2327 [formerly 0163893bafb22583dc24df615fb5855b81eccf31] [formerly b47bf6165d7e1dc2c97ae63fb729dec4b6d7bf15] [formerly 59af39a78d06d86e87a3c068e45feec62821da4b]
    Former-commit-id: 3066becc5134788c206f896c311d5b71348aa052
    Former-commit-id: 574e188e5de9d03f790fdc860ea997897c25693b
    Former-commit-id: 082b575dd7840178764f2053caf669d5d5167a2e
    Former-commit-id: 47156913044daabc56865db0f813e2fd97007587

--- ciphey/neuralNetworkMod/nn.py
+++ ciphey/neuralNetworkMod/nn.py
@@ -22,9 +22,9 @@ import sys
 
 sys.path.append("..")
 try:
-    import mathsHelper as mh
-except ModuleNotFoundError:
     import ciphey.mathsHelper as mh
+except ModuleNotFoundError:
+    import mathsHelper as mh
 
 # i need the below code to make tensorflow shut up. Yup, it's SO bad you have to have 2 LINES TO MAKE IT SHUT UP!!!
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
