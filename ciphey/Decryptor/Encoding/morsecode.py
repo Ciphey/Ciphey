0182018430624d915cb83e03fcbe849b31ef405d
Brandon <brandonskerritt51@gmail.com>
2020-06-08 12:56:30 +0100

    Cleaned up the root folder
    Former-commit-id: 2fd87a683824a29849cd833427c08b91f97f0d5a

--- ciphey/Decryptor/Encoding/morsecode.py
+++ ciphey/Decryptor/Encoding/morsecode.py
@@ -1,53 +1,11 @@
 from loguru import logger
-
+import cipheydists
 
 class MorseCode:
     def __init__(self, lc):
         self.lc = lc
         self.ALLOWED = [".", "-", " ", "/", "\n"]
-        self.MORSE_CODE_DICT = {
-            "A": ".-",
-            "B": "-...",
-            "C": "-.-.",
-            "D": "-..",
-            "E": ".",
-            "F": "..-.",
-            "G": "--.",
-            "H": "....",
-            "I": "..",
-            "J": ".---",
-            "K": "-.-",
-            "L": ".-..",
-            "M": "--",
-            "N": "-.",
-            "O": "---",
-            "P": ".--.",
-            "Q": "--.-",
-            "R": ".-.",
-            "S": "...",
-            "T": "-",
-            "U": "..-",
-            "V": "...-",
-            "W": ".--",
-            "X": "-..-",
-            "Y": "-.--",
-            "Z": "--..",
-            "?": "..--..",
-            ".": ".-.-.-",
-            " ": "/",
-            "0": "-----",
-            "1": ".----",
-            "2": "..---",
-            "3": "...--",
-            "4": "....-",
-            "5": ".....",
-            "6": "-....",
-            "7": "--...",
-            "8": "---..",
-            "9": "----.",
-            " ": "\n",
-        }
-
+        self.MORSE_CODE_DICT = dict(cipheydists.get_charset("morse"))
         self.MORSE_CODE_DICT_INV = {v: k for k, v in self.MORSE_CODE_DICT.items()}
 
     def decrypt(self, text):
