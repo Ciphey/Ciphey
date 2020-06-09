50b576e9bdde1743ba47ab3f8624c21f0a7ebba2
Cyclic3 <cyclic3.git@gmail.com>
2020-06-09 20:31:53 +0100

    Tests now fail correctly

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,13 @@
-from ciphey.__main__ import main
+from ciphey.__main__ import main, make_default_config
 
 
 def test_argument_grep_true():
-    result = main(text="It was the best of times, it was the worst of times")
+    result = main(make_default_config("It was the best of times, it was the worst of times"))
     assert result == "It was the best of times, it was the worst of times"
 
 
 def test_main_base64_true():
-    result = main(
-        text="SXQgd2FzIHRoZSBiZXN0IG9mIHRpbWVzLCBpdCB3YXMgdGhlIHdvcnN0IG9mIHRpbWVzLiBUaGVyZSBpcyBvbmx5IHNvIG11Y2ggcm9hZCBpbiBEb3ZlciBvbmUgY2FuIGxheS4=",
-        greppable=True,
-    )
+    result = main(make_default_config("SXQgd2FzIHRoZSBiZXN0IG9mIHRpbWVzLCBpdCB3YXMgdGhlIHdvcnN0IG9mIHRpbWVzLiBUaGVyZSBpcyBvbmx5IHNvIG11Y2ggcm9hZCBpbiBEb3ZlciBvbmUgY2FuIGxheS4="))
     assert (
-        result["Plaintext"]
-        == "It was the best of times, it was the worst of times. There is only so much road in Dover one can lay."
+        result == "It was the best of times, it was the worst of times. There is only so much road in Dover one can lay."
     )
