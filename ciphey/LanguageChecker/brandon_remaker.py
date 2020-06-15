7e108ea01f1f842ca079d7aab37e8efa795995ad
Brandon <brandonskerritt51@gmail.com>
2020-06-16 00:24:24 +0100

    Started fleshing out the builder

--- ciphey/LanguageChecker/brandon_remaker.py
+++ ciphey/LanguageChecker/brandon_remaker.py
@@ -1,4 +1,5 @@
 import cipheydists
+from math import ceil
 
 dictionary_old = set(cipheydists.get_list("english"))
 
@@ -14,7 +15,19 @@ nlp = spacy.load("en_core_web_sm")
 ValueError: [E088] Text of length 86544687 exceeds maximum of 1000000. The v2.x parser and NER models require roughly 1GB of temporary memory per 100,000 characters in the input. This means long texts may cause memory allocation errors. If you're not using the parser or NER, it's probably safe to increase the `nlp.max_length` limit. The limit is in number of characters, so you can check whether your inputs are too long by checking `len(text)`.
 """
 nlp.max_length = 86545000
-doc = nlp(f[0:500])
 
-for token in doc:
-    print(token.text)
+
+howMany = ceil(len(f) / 1000)
+i = 1
+current_location = 0
+while i <= howMany:
+    # TODO when we reach the end of the list, we need to +1 the list. How do?
+    doc = nlp(f[current_location : (howMany if i is not howMany else howMany + 1)])
+
+    i += 1
+
+    tokenised_text = []
+
+    for token in doc:
+        print(token.text)
+        tokenised_text.append(token.text.lower())
