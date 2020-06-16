68642695476d5549ea4894308d87dfe231d1cb7d
Brandon <brandonskerritt51@gmail.com>
2020-06-16 20:04:14 +0100

    More work on fixing up the combined dict
    Former-commit-id: 733cdb1635d5cd3f93f52e6afd10efee0f58b50e

--- ciphey/LanguageChecker/brandon_remaker.py
+++ ciphey/LanguageChecker/brandon_remaker.py
@@ -6,44 +6,40 @@ import spacy
 
 # This is the spell check that Chrome, MacOS, Firefox, Libre & Open office and much more.
 # Basically, it should be good at itis job.
-from spacy_hunspell import spaCyHunSpell
-
 dictionary_old = set(cipheydists.get_list("english"))
 
-f = open("hansard.txt", encoding="ISO-8859-1").read()
-
+# f = open("hansard.txt", encoding="ISO-8859-1").read()
+f = set(open("aspell.txt", "r").readlines())
 
 nlp = spacy.load("en_core_web_sm")
 # uses the mac os version
-hunspell = spaCyHunSpell(nlp, "linux")
-nlp.add_pipe(hunspell)
 # I want to work on f rn
 
 """
 ValueError: [E088] Text of length 86544687 exceeds maximum of 1000000. The v2.x parser and NER models require roughly 1GB of temporary memory per 100,000 characters in the input. This means long texts may cause memory allocation errors. If you're not using the parser or NER, it's probably safe to increase the `nlp.max_length` limit. The limit is in number of characters, so you can check whether your inputs are too long by checking `len(text)`.
 """
-nlp.max_length = 86545000
+# nlp.max_length = 86545000
 
 
-howMany = ceil(len(f) / 1000)
-current_location = 0
-with alive_bar(howMany) as bar:
-    for i in range(0, howMany):
-        # TODO when we reach the end of the list, we need to +1 the list. How do?
-        doc = nlp(f[current_location : (howMany if i is not howMany else howMany + 1)])
+# howMany = ceil(len(f) / 1000)
+# current_location = 0
+# with alive_bar(howMany) as bar:
+#     for i in range(0, howMany):
+#         # TODO when we reach the end of the list, we need to +1 the list. How do?
+#         doc = nlp(f[current_location : (howMany if i is not howMany else howMany + 1)])
 
-        i += 1
-        current_location += howMany
-        bar()
+#         i += 1
+#         current_location += howMany
+#         bar()
 
-        tokenised_text = []
+#         tokenised_text = []
 
-        for token in doc:
-            print(token.text)
-            if len(token) > 1:
-                # so we dont add stuff like "(" to the dictionary or single letters
-                # as they are not useful
-                tokenised_text.append(token.text.lower())
+#         for token in doc:
+#             print(token.text)
+#             if len(token) > 1:
+#                 # so we dont add stuff like "(" to the dictionary or single letters
+#                 # as they are not useful
+#                 tokenised_text.append(token.text.lower())
 
 # TODO
 """
@@ -63,11 +59,12 @@ then do the same for the real dict
 
 # since they are sets we get uniques only
 complete = f.union(dictionary_old)
-
+print(f"Complete before set union is size {len(f) + len(dictionary_old)}")
+print(f"Now complete size after union is {len(complete)}")
+print(f"The intersection is {f.intersection(dictionary_old)}")
 # turns it all into lowercase
 complete = [word.lower() for word in complete]
 
 # This shouldr remove all words that are of length 1
-complete = filter(len(word) <= 1 for word in complete)
-
-
+complete = list(filter(lambda x: len(x) > 1, complete))
+print(f"Complete size after removing small chars is {len(complete)}")
