1650a0fe56e899246137b7d4d143a7ed097139c3
Brandon <brandonskerritt51@gmail.com>
2020-07-06 11:34:23 +0100

    Added new logo

---
+++ ciphey/basemods/Searchers/imperfection.py
@@ -0,0 +1,38 @@
+from abc import abstractmethod
+from typing import Set, Any, Union, List, Optional, Dict, Tuple
+
+from loguru import logger
+
+from .ausearch import Node, AuSearch
+from ciphey.iface import (
+    SearchLevel,
+    Config,
+    registry,
+    CrackResult,
+    Searcher,
+    ParamSpec,
+    Decoder,
+    DecoderComparer,
+)
+
+import bisect
+
+
+class imperfection(AuSearch):
+    """The default search module for Ciphey
+
+    Called imperfection because ironically it is pretty perfect.
+
+    """
+    @staticmethod
+    def getParams() -> Optional[Dict[str, ParamSpec]]:
+        pass
+
+    def findBestNode(self, nodes: Set[Node]) -> Node:
+        return next(iter(nodes))
+
+    def __init__(self, config: Config):
+        super().__init__(config)
+
+
+registry.register(imperfection, Searcher)
