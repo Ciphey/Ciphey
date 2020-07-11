788d0af2c12d26203baf93777a18f8ad57245be6
ciphey <ciphey@protonmail.com>
2020-07-11 14:51:18 +0100

    not sure why pytest is failing

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,15 @@
-def test():
-    return True
+from ciphey.__main__ import main
+from click.testing import CliRunner
+
+
+def test_initial():
+    runner = CliRunner()
+    # result = runner.invoke(main, ["hello my name is bee and i like bees"])
+    result = runner.invoke(main, ["SGVsbG8gbXkgbmFtZSBpcyBiZWUgYW5kIEkgbGlrZSBkb2cgYW5kIGFwcGxlIGFuZCB0cmVl", "-vvv"])
+    print(result)
+    print(result.output)
+    assert result.exit_code == 0
+    assert "dog" in result.output 
+
+
+test_initial()
