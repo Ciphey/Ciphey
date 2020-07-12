1befd95c03be6bad0cc6bae7ab3d5a8126002dc8
ciphey <ciphey@protonmail.com>
2020-07-12 10:54:07 +0100

    fixing CI

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,18 @@
-from ciphey.__main__ import main
+from ciphey.ciphey import main
 from click.testing import CliRunner
 
 
 def test_initial():
     runner = CliRunner()
     # result = runner.invoke(main, ["hello my name is bee and i like bees"])
-    result = runner.invoke(main, ["SGVsbG8gbXkgbmFtZSBpcyBiZWUgYW5kIEkgbGlrZSBkb2cgYW5kIGFwcGxlIGFuZCB0cmVl", "-vvv"])
+    result = runner.invoke(
+        main,
+        [
+            "SGVsbG8gbXkgbmFtZSBpcyBiZWUgYW5kIEkgbGlrZSBkb2cgYW5kIGFwcGxlIGFuZCB0cmVl",
+            "-vvv",
+        ],
+    )
     print(result)
     print(result.output)
     assert result.exit_code == 0
-    assert "dog" in result.output 
-
-
+    assert "dog" in result.output
