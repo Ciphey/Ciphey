842a3b4f65deb6fce4dd2233de23d56dd6d87f07
ciphey <ciphey@protonmail.com>
2020-07-21 23:14:51 +0100

    trying CI

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,7 +9,7 @@ def test_initial():
         main,
         [
             "SGVsbG8gbXkgbmFtZSBpcyBiZWUgYW5kIEkgbGlrZSBkb2cgYW5kIGFwcGxlIGFuZCB0cmVl",
-            "-vvv",
+            "-q",
         ],
     )
     print(result)
@@ -26,8 +26,8 @@ def test_base16():
         main,
         [
             "JBSWY3DPEBWXSIDOMFWWKIDJOMQGEZLFEBQW4ZBAJEQGY2LLMUQGI33HEBQW4ZBAMNQXI===",
-            "-vvv",
-        ],
+            "-q",
+        ]
     )
     assert result.exit_code == 0
     assert "dog" in result.output
@@ -36,7 +36,7 @@ def test_base16():
 def test_caesar():
     runner = CliRunner()
     result = runner.invoke(
-        main, ["Uryyb zl anzr vf orr naq V yvxr qbt naq png", "-vvv"],
+        main, ["Uryyb zl anzr vf orr naq V yvxr qbt naq png", "-q"],
     )
     assert result.exit_code == 0
     assert "dog" in result.output
@@ -48,7 +48,7 @@ def test_base64_caesar():
         main,
         [
             "01010011 01000111 01010110 01110011 01100010 01000111 00111000 01100111 01100010 01011000 01101011 01100111 01100010 01101101 01000110 01110100 01011010 01010011 01000010 01110000 01100011 01111001 01000010 01101001 01011010 01010111 01010101 01100111 01011001 01010111 00110101 01101011 01001001 01000101 01101011 01100111 01100010 01000111 01101100 01110010 01011010 01010011 01000010 01101011 01100010 00110010 01100011 01100111 01011001 01010111 00110101 01101011 01001001 01000111 01001110 01101000 01100100 01000011 01000010 01101001 01100100 01011000 01010001 01100111 01010011 01010011 01000010 01101000 01100010 01001000 01001110 01110110 01001001 01000111 01010010 01110110 01001001 01000111 01101000 01101000 01100011 01001000 01000010 01101100 01100010 01101001 01000010 00110000 01100010 01111001 01000010 01101100 01100010 01101101 01110000 01110110 01100101 01010011 01000010 00110011 01011001 01010111 01111000 01110010 01100011 01110111 00111101 00111101",
-            "-vvv",
+            "-q",
         ],
     )
     assert result.exit_code == 0
@@ -58,7 +58,7 @@ def test_base64_caesar():
 def test_vigenere():
     runner = CliRunner()
     result = runner.invoke(
-        main, ["Omstv uf vhul qz jlm hvk Q sqrm kwn iul jia", "-vvv"],
+        main, ["Omstv uf vhul qz jlm hvk Q sqrm kwn iul jia", "-q"],
     )
     assert result.exit_code == 0
     assert "dog" in result.output
@@ -70,7 +70,7 @@ def test_binary():
         main,
         [
             "01001000 01100101 01101100 01101100 01101111 00100000 01101101 01111001 00100000 01101110 01100001 01101101 01100101 00100000 01101001 01110011 00100000 01100010 01100101 01100101 00100000 01100001 01101110 01100100 00100000 01001001 00100000 01101100 01101001 01101011 01100101 00100000 01100100 01101111 01100111 00100000 01100001 01101110 01100100 00100000 01100011 01100001 01110100",
-            "-vvv",
+            "-q",
         ],
     )
     assert result.exit_code == 0
@@ -83,7 +83,7 @@ def test_hex():
         main,
         [
             "48 65 6c 6c 6f 20 6d 79 20 6e 61 6d 65 20 69 73 20 62 65 65 20 61 6e 64 20 49 20 6c 69 6b 65 20 64 6f 67 20 61 6e 64 20 63 61 74",
-            "-vvv",
+            "-q",
         ],
     )
     assert result.exit_code == 0
