5ae8a596d2dc914cd763f5ff0ae651c2e002162a
Lukas Gabriel <52338810+lukasgabriel@users.noreply.github.com>
2020-08-27 11:36:36 +0200

    Move atbash alphabet to cipheydists
    Move atbash alphabet to cipheydists and add a parameter to specify which alphabet is used for the atbash operation.

--- ciphey/basemods/Decoders/atbash.py
+++ ciphey/basemods/Decoders/atbash.py
@@ -3,7 +3,7 @@
 
 from typing import Optional, Dict, List
 
-from ciphey.iface import Config, ParamSpec, T, U, Decoder, registry
+from ciphey.iface import Config, ParamSpec, T, U, Decoder, registry, WordList
 
 
 @registry.register
@@ -18,8 +18,7 @@ class Atbash(Decoder[str, str]):
         """
 
         result = ""
-        letters = list("abcdefghijklmnopqrstuvwxyz")
-        atbash_dict = {letters[i]: letters[::-1][i] for i in range(26)}
+        atbash_dict = {self.ALPHABET[i]: self.ALPHABET[::-1][i] for i in range(26)}
 
         # Ensure that ciphertext is a string
         if type(ctext) == str:
@@ -45,10 +44,17 @@ class Atbash(Decoder[str, str]):
 
     def __init__(self, config: Config):
         super().__init__(config)
+        self.ALPHABET = config.get_resource(self._params()["dict"], WordList)
 
     @staticmethod
     def getParams() -> Optional[Dict[str, ParamSpec]]:
-        return None
+        return {
+            "dict": ParamSpec(
+                desc="The alphabet used for the atbash operation.",
+                req=False,
+                default="cipheydists::list::englishAlphabet",
+            )
+        }
 
     @staticmethod
     def getTarget() -> str:
