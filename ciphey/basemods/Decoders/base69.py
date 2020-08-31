4780997ee78a32cfb238ba85a957830282736f3d
Lukas Gabriel <52338810+lukasgabriel@users.noreply.github.com>
2020-08-31 07:33:31 +0200

    Move test code to main file

--- ciphey/basemods/Decoders/base69.py
+++ ciphey/basemods/Decoders/base69.py
@@ -16,19 +16,27 @@ class Base69(Decoder[str, str]):
         return f'{self.CHARS[n % 69]}{self.CHARS[floor(n / 69)]}'
 
     def chars_to_byte(self, s: str):
+        print(f'c2b received {s} and returns {(69 * self.CHARS.index(s[1])) + (self.CHARS.index(s[0]))}')
         return (69 * self.CHARS.index(s[1])) + (self.CHARS.index(s[0]))
 
     def decode_chunk(self, s: str):
         padded_bytes = s.endswith('=')
-        decoded = []
+        decoded = [0 for _ in range(8)]
         for i in range(8):
-            decoded[i] = 0 if i == 7 and padded_bytes else chars_to_byte(s[ i * 2 : i * 2 + 2 ])
-        result = []
-        for i in range(8):
+            decoded[i] = 0 if i == 7 and padded_bytes else self.chars_to_byte(s[ i * 2 : i * 2 + 2 ])
+            print(f'decoded[{i}] = {decoded[i]}')
+        result = [0 for _ in range(7)]
+        for i in range(7):
             t1 = decoded[i] << (i + 1)
+            print(f't1 = {decoded[i]} << {(i + 1)}')
+            print(f't1 is {t1}')
             t2 = decoded[i + 1] >> (7 - i - 1)
+            print(f't2 is {t2}')
+            print(f't2 = {decoded[i + 1]} >> {(7 - i - 1)}')
             result[i] = t1 | t2
+            print(f'result[{i}] = {t1 | t2}')
         return result
+
 
     def decode(self, ctext: T) -> Optional[U]:
         """
@@ -36,17 +44,23 @@ class Base69(Decoder[str, str]):
         """
         extra_bytes = 0
         clen = len(ctext)
-        if ctext[clen - 1] == '=':
-            extra_bytes = ctext[clen - 2]
+        if ctext[:-1] == '=':
+            extra_bytes = int(ctext[clen - 2])
         CHUNK_COUNT = ceil(clen / 16)
-        result = []
+        result = [0 for _ in range(CHUNK_COUNT * 7 - extra_bytes)]
         for i in range(CHUNK_COUNT):
             chunk_string = ctext[i * 16 : (i + 1) * 16]
             if extra_bytes and (i == CHUNK_COUNT - 1):
-                result[self.decode_chunk(chunk_string)[0: 7 - extra_bytes]] = i * 7
+                insert = self.decode_chunk(chunk_string)
+                for n, elem in enumerate(insert[0 : 7 - extra_bytes]):
+                    result[n + i * 7] = elem
             else:
-                result[self.decode_chunk(chunk_string)] =  i * 7
-        return result
+                insert = self.decode_chunk(chunk_string)
+                for n, elem in enumerate(insert):
+                    result[n + i * 7] = elem
+
+        print(result)
+        return bytearray(result).decode()
 
 
     @staticmethod
