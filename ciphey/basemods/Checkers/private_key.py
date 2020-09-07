61860486f4c64716efa8d97b85390d72c0a049d3
Cyclic3 <cyclic3.git@gmail.com>
2020-09-07 20:32:56 +0100

    Added private key checker

---
+++ ciphey/basemods/Checkers/private_key.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from ciphey.iface import PrivateKey, registry, Checker, Config
+
+
+@registry.register
+class PrivateKeyChecker(Checker[PrivateKey]):
+    def getExpectedRuntime(self, text: PrivateKey) -> float:
+        return 0
+
+    def __init__(self, config: Config):
+        super().__init__(config)
+
+    def check(self, text: PrivateKey) -> Optional[str]:
+        return ""
+
+    @staticmethod
+    def getParams() -> Optional[Dict[str, ParamSpec]]:
+        pass
--- ciphey/basemods/Decoders/pem.py
+++ ciphey/basemods/Decoders/pem.py
@@ -1,58 +1,54 @@
-from typing import Optional, Dict
+import types
+from typing import Optional, Dict, Callable, Any
 
-from cryptography.hazmat.primitives.serialization import load_pem_private_key
-from cryptography.x509 import load_pem_x509_certificate
+from cryptography.hazmat.primitives.serialization import load_pem_private_key, load_pem_public_key, \
+    load_der_private_key, load_der_public_key
+from cryptography.x509 import load_pem_x509_certificate, load_der_x509_certificate
 from cryptography.hazmat.backends import default_backend
 import cryptography.exceptions
+from loguru import logger
 
-from ciphey.iface import Decoder, Der, Config, X509, ParamSpec
+from ciphey.common import id_lambda
+from ciphey.iface import Decoder, PrivateKey, PublicKey, Config, X509, ParamSpec, registry
 
-
-class PemDer(Decoder[str, Der]):
-    @staticmethod
-    def getParams() -> Optional[Dict[str, ParamSpec]]:
-        pass
-
-    @staticmethod
-    def getTarget() -> str:
-        return "pem"
-
-    def decode(self, ctext: str) -> Optional[Der]:
-        try:
-            return load_pem_private_key(ctext, None, backend=default_backend())
-        except ValueError:
-            pass
-        except cryptography.exceptions.UnsupportedAlgorithm:
-            pass
-
-    @staticmethod
-    def priority() -> float:
-        return 0.01
-
-    def __init__(self, config: Config):
-        super().__init__(config)
+backend = default_backend()
 
 
-class PemX509(Decoder[str, X509]):
-    @staticmethod
-    def getParams() -> Optional[Dict[str, ParamSpec]]:
-        pass
+def _dispatch(self: Any, ctext: str, func: Callable):
+    logger.trace(f"Attempting {self.getTarget()}")
+    try:
+        result = func(ctext)
+        logger.debug(f"{self.getTarget()} successful, returning {result}")
+        return result
+    except ValueError:
+        logger.trace(f"Failed to decode {self.getTarget()}")
+    except cryptography.exceptions.UnsupportedAlgorithm:
+        logger.trace(f"Unknown key type {self.getTarget()}")
 
-    @staticmethod
-    def getTarget() -> str:
-        return "pem"
+_decoders = {
+    "pem_private": ((lambda x: load_pem_private_key(x.encode(), None, backend=backend)), 0.01, str, PrivateKey),
+    "pem_public": ((lambda x: load_pem_public_key(x.encode(), backend=backend)), 0.01, str, PublicKey),
+    "pem_x509": ((lambda x: load_pem_public_key(x.encode(), backend=backend)), 0.01, str, X509),
+    "der_private": ((lambda x: load_der_private_key(x.encode(), None, backend=backend)), 0.01, str, bytes),
+    "der_public": ((lambda x: load_der_public_key(x.encode(), backend=backend)), 0.01, str, bytes),
+    "der_x509": ((lambda x: load_der_x509_certificate(x.encode(), backend=backend)), 0.01, str, X509),
+}
 
-    def decode(self, ctext: str) -> Optional[Der]:
-        try:
-            return load_pem_private_key(ctext, None, backend=default_backend())
-        except ValueError:
-            pass
-        except cryptography.exceptions.UnsupportedAlgorithm:
-            pass
 
-    @staticmethod
-    def priority() -> float:
-        return 0.01
+def gen_class(name, decoder, priority, ns):
+    ns["_get_func"] = id_lambda(decoder)
+    ns["decode"] = lambda self, ctext: _dispatch(self, ctext, self._get_func())
+    ns["getParams"] = id_lambda(None)
+    ns["getTarget"] = id_lambda(name)
+    ns["priority"] = id_lambda(priority)
+    ns["__init__"] = lambda self, config: super(type(self), self).__init__(config)
 
-    def __init__(self, config: Config):
-        super().__init__(config)
+
+for name, (decoder, priority, src, dst) in _decoders.items():
+    t = types.new_class(
+        name,
+        (Decoder[src, dst],),
+        exec_body=lambda x: gen_class(name, decoder, priority, x),
+    )
+
+    registry.register(t)
