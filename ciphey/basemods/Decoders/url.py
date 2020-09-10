1d18fdac9c2e9d642b8b18ac4a1747bc4cea863f
MikeMerzl <mikemerzl13@gmail.com>
2020-09-10 15:03:09 +0300

    changed name, added simple test

--- ciphey/basemods/Decoders/url.py
+++ ciphey/basemods/Decoders/url.py
@@ -7,9 +7,8 @@ from ciphey.iface import ParamSpec, Config, T, U, Decoder, registry
 @registry.register_multi((str, str), (bytes, bytes))
 class URL(Decoder[T, U]):
     def decode(self, ctext: T) -> Optional[U]:
-        """Write the code that decodes here
-        ctext -> the input to the function
-        returns string"""
+        """Given a string the function will try and decode URL coding,
+        returns string if indeed URL coding else returns None"""
         ctext = ctext.replace("+", " ")  # replace + sign with spacebar for unquote
         result = unquote(ctext)  #built-in function that decodes URL coding
         if ctext != result:
