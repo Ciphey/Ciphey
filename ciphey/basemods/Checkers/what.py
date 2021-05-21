9df16a2586b32c494ec8b78e621441220bd56a4b
Brandon <github@skerritt.blog>
2021-05-21 19:28:54 +0100

    Made Cipheys output more friendly and fixed the what bug (#632)

--- ciphey/basemods/Checkers/what.py
+++ ciphey/basemods/Checkers/what.py
@@ -3,6 +3,9 @@ from typing import Dict, Optional
 from ciphey.iface import Checker, Config, ParamSpec, T, registry
 from loguru import logger
 from pywhat import identifier
+from rich.console import Console
+
+console = Console()
 
 
 @registry.register
@@ -16,6 +19,7 @@ class What(Checker[str]):
         logger.trace("Trying PyWhat checker")
         returned_regexes = self.id.identify(ctext, api=True)
         if len(returned_regexes["Regexes"]) > 0:
+            console.print(f'\nI think the plaintext is a [yellow]{returned_regexes["Regexes"][0]["Regex Pattern"]["Name"]}[/yellow]')
             return returned_regexes["Regexes"][0]["Regex Pattern"]["Name"]
         return None
 
