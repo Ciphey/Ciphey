7c312dc348f53caab31afcaff0116a1560f077f5
bee-san <github@skerritt.blog>
2021-06-06 09:25:34 +0100

    Fixing skeletals PR feedback

--- ciphey/__main__.py
+++ ciphey/__main__.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         + str(sys.version_info[2])
     )
 
-    if major != 3 or (major == 3 and minor < 6):
+    if major != 3 or minor < 6:
         print(
             f"Ciphey requires Python 3.6+, you are using {python_version}. Please install a higher Python version. https://www.python.org/downloads/"
         )
@@ -29,12 +29,8 @@ if __name__ == "__main__":
         if minor > 8:
             print("Ciphey does not currently support Python 3.9 on Windows. Please use the Discord bot at http://discord.skerritt.blog")
             sys.exit(1)
-        # Not in the main imports because I don't fancy importing an entire module
-        # For no reason
-        import struct
 
-        # see https://marcellodelbono.it/en/detecting-if-python-is-running-as-a-64-bit-application
-        if struct.calcsize("P") * 8 == 32:
+        if sys.maxsize > 2**32 == False:
             print("You are using Python 32 bit and Windows, Ciphey does not support this. Please upgrade to Python 64-bit here https://www.python.org/downloads/")
             sys.exit(1)
     from .ciphey import main
